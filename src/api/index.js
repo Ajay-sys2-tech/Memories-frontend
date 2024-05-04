a36891fff58f8e641f06d463eb5cb36723909171
ajay4985622 <ajay@vitto.money>
2024-05-05 00:13:35 +0530

    added authentiation

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
 
-const url = 'https://memories-api-603q.onrender.com/posts';
+
+const url = 'https://memories-api-603q.onrender.com';
+// const url = 'http://localhost:5000';
+
+const API = axios.create({ baseURL: url });
+
+API.interceptors.request.use((req) => {
+    if(localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+})
 
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id ) => axios.patch(`${url}/${id}/likePost`);
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id ) => API.patch(`/posts/${id}/likePost`);
+
+export const signIn = ( fromData ) => API.post( '/users/signin', fromData );
+export const signUp = ( fromData ) => API.post( '/users/signup', fromData );
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,7 +8,6 @@ import useStyles from './styles';
 
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData ] = useState({
-      creator: '',
       title: '',
       message: '',
       tags: '',
@@ -18,6 +17,7 @@ const Form = ({ currentId, setCurrentId }) => {
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
       if(post) setPostData(post);
@@ -27,22 +27,29 @@ const Form = ({ currentId, setCurrentId }) => {
       e.preventDefault();
 
       if(currentId){
-        dispatch(updatePost(currentId, postData));
+        dispatch(updatePost(currentId, { ...postData, name: user?.result?.name}));
       }else{
-        dispatch(createPost(postData));
+        dispatch(createPost({ ...postData, name: user?.result?.name}));
       }
       clear();
     }
 
     const clear = () => {
       setCurrentId(null);
-      setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+      setPostData({ title: '', message: '', tags: '', selectedFile: '' })
+    }
+
+    if(!user?.result?.name){
+      return (
+        <Paper className={classes.paper}>
+          <Typography variant="h6" align="center">Please sign in to create a new Post!</Typography>
+        </Paper>
+      )
     }
   return (
     <Paper className={classes.paper}>
       <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant='h6'>{currentId ? 'Editing' : 'Creating' } a memory</Typography>
-        <TextField name='creator' variant='outlined' label='creator' fullWidth value={postData.creator} onChange={(e) => setPostData({...postData, creator: e.target.value})} />
         <TextField name='title' variant='outlined' label='title' fullWidth value={postData.title} onChange={(e) => setPostData({...postData, title: e.target.value})} />
         <TextField name='message' variant='outlined' label='message' fullWidth value={postData.message} onChange={(e) => setPostData({...postData, message: e.target.value})} />
         <TextField name='tags' variant='outlined' label='tags' fullWidth value={postData.tags} onChange={(e) => setPostData({...postData, tags: e.target.value.split(',')})} />
